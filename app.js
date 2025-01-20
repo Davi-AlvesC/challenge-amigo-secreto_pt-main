34ce0add04763be254f0c1affd3de154ad83abb1
Davi Alves da Costa <davi4076018@gmail.com>
2025-01-20 18:31:52 -0300

    Funcionalidades do card

--- app.js
+++ app.js
@@ -17,10 +17,40 @@ function adicionarAmigo() {
 
 function atualizarLista(item) {
     let lista = document.getElementById('listaAmigos');
-    //lista.innerHTML = "";
 
-    // Criando o item da lista
+    // Criando o item da lista 
     let itemLista = document.createElement('li');
-    itemLista.textContent = item;
+    itemLista.onclick = function () {
+        removerAmigo(itemLista);
+    };
+    let lixeiraIcon = document.createElement('i');
+    lixeiraIcon.classList.add('bi');
+    lixeiraIcon.classList.add('bi-trash3-fill');
+    lixeiraIcon.classList.add('trash');
+    
+
+    let naipeIcon = document.createElement('i');
+    naipeIcon.classList.add('bi');
+    naipeIcon.classList.add('bi-suit-spade-fill');
+    naipeIcon.classList.add('naipeTopo');
+
+    let texto = document.createElement('a');
+    texto.classList.add('texto-card');
+    texto.textContent = item;
+    itemLista.appendChild(texto);
+    itemLista.appendChild(lixeiraIcon)
+    itemLista.appendChild(naipeIcon)
     lista.appendChild(itemLista);
+}
+
+// Função para remover um amigo da lista
+function removerAmigo(item) {
+    // Remover o item da lista
+    amigos.splice(amigos.indexOf(item.querySelector('a').innerHTML), 1);
+    item.remove();
+    console.log(amigos);
+}
+
+function sortearAmigo() {
+    console.log('sorteou')
 }
